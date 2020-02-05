007ff674a74a9c3d0e83e0d4596d9f1266dc681d
Tareq Hasan <tareq1988@gmail.com>
2020-02-05 16:33:55 +0800

    Fix WP SSL typo

--- cli/bediq.php
+++ cli/bediq.php
@@ -363,27 +363,27 @@ $app->command('db:export domain', function ($domain) {
 })->descriptions('Export database.');
 
 $app->command('site:ssl static_url wp_url', function ($static_url, $wp_url) {
-    $cli = new CommandLine();
-    $file = new Filesystem();
+    $cli   = new CommandLine();
+    $file  = new Filesystem();
     $nginx = new Nginx($cli, $file);
-    $lxd = new Lxc($cli, $file);
+    $lxd   = new Lxc($cli, $file);
 
     $container = $lxd->nameByDomain($static_url);
     $ip = $lxd->getIp($container);
 
     // apply ssl for static
     output("Adding certificate for {$static_url}...");
-    $cli->run('certbot certonly -d '.$static_url.'  --nginx');
+    $cli->run('certbot certonly -d ' . $static_url . '  --nginx');
     $nginx->applySSL($static_url);
 
     // apply ssl for staging
     output("Adding certificate for {$wp_url}...");
-    $cli->run('certbot certonly -d staging-'.c.'  --nginx');
+    $cli->run('certbot certonly -d staging-' . $wp_url . '  --nginx');
     $nginx->applySSL($wp_url, $ip);
 
     $nginx->reloadNginx();
 
-    output('SSL applied to '.$static_url.' and '.$wp_url);
+    output('SSL applied to ' . $static_url . ' and ' . $wp_url);
 
 })->descriptions('SSL certificate install.');
 
